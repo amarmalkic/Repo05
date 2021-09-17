edb30c1ef54dc8bbdc80bb098cf7fa84f46e69da
Amar Malkic <amar.malkic96@gmail.com>
2021-09-18 01:55:31 +0200

    +ex for day 5

---
+++ ex14/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    // Only change code below this line
+    var myNoun = "dog";
+    var myVerb ="ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = "My " + myAdjective + ", ugly " + myNoun + " never " + myVerb + " that " + myAdverb + ".";
+    // Only change code above this line
+    return wordBlanks;
+}
+console.log(main());
+module.exports = main;
